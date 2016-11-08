d6c7d4767f0bfb5528a3d6c9c24011e5970b8227
ccataldo345 <christian.cataldo@itcollege.ee>
2016-11-08 15:44:45 +0200

    Create Project2016 and added first Main file

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Chris01 on 08/11/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Project by Christian Cataldo, IT College Tallinn, Cyber Security Engineering, 2016");
+    }
+}
