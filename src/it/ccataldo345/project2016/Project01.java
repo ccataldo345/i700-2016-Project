b4df0ef81a9305c2c7ecf20efc68cfa7d435f9fc
ccataldo345 <christian.cataldo@itcollege.ee>
2016-11-25 11:27:57 +0200

    Clean up code and set new initial Class (ask for name and initial credit) with collector array user

--- src/it/ccataldo345/project2016/Project01.java
+++ src/it/ccataldo345/project2016/Project01.java
@@ -5,55 +5,28 @@
 package it.ccataldo345.project2016;
 
 import lib.TextIO;
+import java.util.ArrayList;
 
 public class Project01 {
     public static void main(String[] args) {
-        System.out.println("Project by Christian Cataldo, IT College Tallinn, Cyber Security Engineering, 2016");
-        Account personAccount = getPersonData();
-        double gain = personAccount.calculateGain();
-        System.out.println("Name: " + personAccount.getName());
-        System.out.println("Credit: " + personAccount.getMoney() + " Euros.");
-        System.out.println(personAccount.getName() + " will earn " + gain + " Euros in " + personAccount.getPeriod() + " years!");
+        System.out.println("Project by Christian Cataldo, IT College Tallinn, CSE group C11, 2016");
+        System.out.println("Welcome to Java online store");
+        //Create an arrayList to store the user
+        ArrayList<Account> user = new ArrayList<>();
+
+
+        System.out.printf("Please enter a name:\n");
+        String name = TextIO.getlnString();
+
+        System.out.printf("Please enter your initial credit you want to deposit: \n");
+        double credit = TextIO.getlnDouble();
+
+        Account newAccount = new Account(name, credit);
+        user.add(newAccount);
+
+
+        for (Account account : user) {
+            account.greet();
+        }
     }
-
-    public static Account getPersonData(){
-        Account client = new Account();
-        System.out.println("Please enter your name: ");
-        client.setName(TextIO.getlnString());
-
-        System.out.println("Please enter amount in Euros: ");
-        client.setMoney(TextIO.getlnDouble());
-
-        System.out.println("Please enter time lenght of deposit in years: ");
-        client.setPeriod(TextIO.getlnDouble());
-        
-        return client;
-    }
-
-
-/*    old code:
-
-        public static String name() {
-
-        System.out.println("Please enter your name: ");
-        ben.setName(TextIO.getlnString());
-    }*/
-
-/*    public static double money() {
-        System.out.println("Please enter amount in Euros: ");
-        double money01 = TextIO.getlnDouble();
-        return money01;
-    }*/
-
-/*    public static double period() {
-        System.out.println("Please enter time lenght of deposit in years: ");
-        double time01 = TextIO.getlnDouble();
-        return time01;
-    }
-
-    public static double gain() {
-        double yield01 = 0.035;
-        double gain01 = money() * period() * yield01;
-        return gain01;
-    }*/
 }
