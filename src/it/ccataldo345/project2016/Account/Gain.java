b3591b170ceb415408ec6f24cb2d54fc47551b41
ccataldo345 <christian.cataldo@itcollege.ee>
2016-12-01 17:44:21 +0200

    Implemented code to Gain (redirection from Account, ask input for period and calculates gain)

--- src/it/ccataldo345/project2016/Account/Gain.java
+++ src/it/ccataldo345/project2016/Account/Gain.java
@@ -7,14 +7,14 @@ package it.ccataldo345.project2016.Account;
 */
 
 
-import it.ccataldo345.project2016.Account.Account;
+import lib.TextIO;
 
 public class Gain {
 
     private double period;
     private double gain;
 
-    private static final double YIELD = 0.035;
+    private static final double YIELD = 0.035;   //CONSTANT value (name with all capital letters!!!)
 
 
     public Gain() {
@@ -31,11 +31,21 @@ public class Gain {
     }
 
 
-    public double calculateGain() {
-        this.gain = this.credit * this.period * YIELD;   //Gain formula
+    public void calculateGain() {
+
+        // set: if balance is 0 > go to InitialCredit;
+
+        System.out.println("Please enter the deposit period of your credit: ");
+        this.period = TextIO.getlnDouble();
+        System.out.printf("Please enter the initialCredit balance you want to deposit: \n");
+
+        this.gain = /*balance * */this.period * YIELD;   //Gain formula
         String round = String.format("%.2f", this.gain);  // set the decimals to 2
         this.gain = Double.valueOf(round);
-        return this.gain;
+
+        System.out.println("If you deposit your credit of ..., you will earn: " + this.gain + " Euro.");
+        System.out.println();
+        return;
     }
 }
 
